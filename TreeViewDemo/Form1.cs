74c970ec06ac0cfc51f8b83e7e84aa1983f2dce9
markforest <toxerius7@gmail.com>
2018-04-01 11:53:39 +0300

    end

--- TreeViewDemo/Form1.cs
+++ TreeViewDemo/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,37 @@ namespace TreeViewDemo
         public Form1()
         {
             InitializeComponent();
+            
             TreeNode tovarNode = new TreeNode("Товары");
+            //добавляем новый дочерний узел к tovarNode
+            TreeNode smartFon = new TreeNode("Смартфон");
+            tovarNode.Nodes.Add(smartFon);
+
+            //string[] items = {"first", "second", "Thirth", "fourth", "fivth" };
+
+            //IEnumerable<string> result = (from item in items where item.StartsWith("f") select item).Distinct();
+            //foreach (string item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Добавляем ветку к treeView
+            this.treeView1.Nodes.Add(tovarNode);
+
+            //Доюовляем второй дочерний узел к первому узлу treeNode
+            treeView1.Nodes[0].Nodes.Add(new TreeNode("Планшеты"));
+
+            //Удаление
+            //treeView1.Nodes[0].Nodes.RemoveAt(1);
+            //treeView1.Nodes.Remove(tovarNode);
+
+            ImageList imageList = new ImageList();
+            //....
+            imageList.Images.Add(Image.FromFile("image.png"));
+
+            treeView1.ImageList = imageList;
+            treeView1.Nodes[0].Nodes.Add(new TreeNode("Планшеты", 0,1));
+            //....
         }
     }
 }
---
+++ TreeViewObhodDemo2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewObhodDemo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            treeView1.BeforeSelect += TreeView1_BeforeSelect;
+            treeView1.BeforeExpand += TreeView1_BeforeExpand;
+
+            //Заполняем дерево дисками
+            fillDriveNodes();
+
+        }
+
+        private void fillDriveNodes()
+        {
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    TreeNode driveNode = new TreeNode(drive.Name);
+                    FillTreeNode(driveNode, drive.Name);
+                    treeView1.Nodes.Add(driveNode);
+
+                }
+                
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (var dir in dirs)
+                {
+
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+
+        private void TreeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            
+        }
+
+        private void TreeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+           
+        }
+    }
+}
