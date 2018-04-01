82c63b6a9dc2be8099206019bf19f200f90ddc3f
markforest <toxerius7@gmail.com>
2018-04-01 11:09:33 +0300

    Initial commit

---
+++ RichTextBoxDemo/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace RichTextBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.identToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bulletToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(466, 318);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.fontToolStripMenuItem,
+            this.identToolStripMenuItem,
+            this.bulletToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.undoToolStripMenuItem,
+            this.сToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 192);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // identToolStripMenuItem
+            // 
+            this.identToolStripMenuItem.Name = "identToolStripMenuItem";
+            this.identToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.identToolStripMenuItem.Text = "Ident";
+            this.identToolStripMenuItem.Click += new System.EventHandler(this.identToolStripMenuItem_Click);
+            // 
+            // bulletToolStripMenuItem
+            // 
+            this.bulletToolStripMenuItem.Name = "bulletToolStripMenuItem";
+            this.bulletToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bulletToolStripMenuItem.Text = "Bullet";
+            this.bulletToolStripMenuItem.Click += new System.EventHandler(this.bulletToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // сToolStripMenuItem
+            // 
+            this.сToolStripMenuItem.Name = "сToolStripMenuItem";
+            this.сToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сToolStripMenuItem.Text = "Redo";
+            this.сToolStripMenuItem.Click += new System.EventHandler(this.сToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 398);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem identToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bulletToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+    }
+}
+
---
+++ RichTextBoxDemo/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RichTextBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.ContextMenuStrip = contextMenuStrip1;
+            richTextBox1.TextChanged += RichTextBox1_TextChanged;
+            undoToolStripMenuItem.Enabled = false;
+            сToolStripMenuItem.Enabled = false;
+            richTextBox1.LinkClicked += RichTextBox1_LinkClicked;
+        }
+
+        private void RichTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            if (MessageBox.Show("What!...") == DialogResult.OK)
+            {
+                Process.Start(e.LinkText);
+            }
+        }
+
+        //В момент изменения текста проверяем есть ли что оменять
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            undoToolStripMenuItem.Enabled = richTextBox1.CanUndo;
+            сToolStripMenuItem.Enabled = richTextBox1.CanRedo;
+        }
+
+        //Открытие файла
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //richTextBox1.Text = @"Lorem Ipsum - це текст-, що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично, стандартною  аж з XVI сторіччя, коли невідомий друкар взяв шрифтову гранку та склав на ній підбірку зразків шрифтів.  не тільки успішно пережила п'ять століть, але й прижилася в електронному верстуванні, залишаючись по суті незмінною. Вона популяризувалась в 60-их роках минулого сторіччя завдяки виданню зразків шрифтів Letraset, які містили уривки з Lorem Ipsum, і вдруге - нещодавно завдяки програмам комп'ютерного верстування на кшталт Aldus Pagemaker, які використовували різні версії Lorem Ipsum.";
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Text Files|*.txt|Rich Text Document|*.rtf|All Files|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                if (dlg.FileName.EndsWith("rtf"))
+                {
+                    richTextBox1.LoadFile(dlg.FileName);
+                }else
+                {
+                    richTextBox1.Text = File.ReadAllText(dlg.FileName);
+                }
+            }
+        }
+
+        //Выбор шрифта выделеного текста
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.Font = richTextBox1.SelectionFont;
+            fontDialog.Color = richTextBox1.SelectionColor;
+            fontDialog.ShowColor = true;
+            if (fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog.Font;
+                richTextBox1.SelectionColor = fontDialog.Color;
+            }
+
+        }
+
+        //Отступ
+        private void identToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionIndent = 20;
+        }
+
+        //Маркировка текста
+        private void bulletToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionBullet = true;
+        }
+
+        //Отменить действие
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+               richTextBox1.Undo();
+        }
+
+        //Отменить отмененое действие
+        private void сToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+               richTextBox1.Redo();
+        }
+
+        //Сохранения в нужном на формате
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = dlg.Filter = "Text Files|*.txt|Rich Text Document|*.rtf|All Files|*.*";
+
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                if (!String.IsNullOrWhiteSpace(dlg.FileName))
+                {
+                    if (dlg.FileName.EndsWith("rtf"))
+                    {
+                        richTextBox1.SaveFile(dlg.FileName, RichTextBoxStreamType.RichText);
+                    }
+                    else
+                    {
+                        richTextBox1.SaveFile(dlg.FileName, RichTextBoxStreamType.PlainText);
+                    }
+                }
+            }
+        }
+    }
+}
